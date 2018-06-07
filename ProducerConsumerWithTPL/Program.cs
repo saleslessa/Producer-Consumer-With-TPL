8ee901431a967fb8ca473d2c2087d35b909edb76
Sales Lopes <lopesdealencar@gmail.com>
2018-06-07 15:42:55 +0200

    First commit

---
+++ ProducerConsumerWithTPL/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProducerConsumerWithTPL
+{
+    class Program
+    {
+		static BlockingCollection<int> messages = new BlockingCollection<int>(new ConcurrentBag<int>(), 10);
+        static CancellationTokenSource cts = new CancellationTokenSource();
+        static Random random = new Random();
+
+        static void Main(string[] args)
+        {
+            Task.Factory.StartNew(RunProducerConsumer);
+            Console.ReadKey();
+            cts.Cancel();         
+        }
+
+        static void RunProducerConsumer()
+        {
+            var producer1 = Task.Factory.StartNew(RunProducer);
+            var producer2 = Task.Factory.StartNew(RunProducer);
+            var consumer = Task.Factory.StartNew(RunConsumer);
+
+            try
+            {
+                Task.WaitAll(new[] { producer1, producer2, consumer }, cts.Token);
+            }
+            catch (AggregateException ae)
+            {
+				ae.Handle(e => 
+				{
+					Console.WriteLine($"Oops! Something wrong happened. Please see errors below:");
+					foreach (var error in ae.InnerExceptions)
+					{
+						Console.WriteLine($"  - {error.Message}");
+					}
+					return true;
+				});
+			}
+        }
+
+        static void RunProducer()
+        {
+            while (true)
+            {
+                cts.Token.ThrowIfCancellationRequested();
+                int i = random.Next(100);
+                messages.Add(i);
+                Console.WriteLine($"Producing {i} from {Task.CurrentId}");
+                Thread.Sleep(random.Next(100));
+            }
+        }
+
+        static void RunConsumer()
+        {
+            foreach (var item in messages.GetConsumingEnumerable())
+            {
+                cts.Token.ThrowIfCancellationRequested();
+                Console.WriteLine($"Consuming {item}\t");
+                Thread.Sleep(random.Next(1000));
+            }
+        }
+    }
+}
